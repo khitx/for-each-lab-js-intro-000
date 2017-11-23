cef637ccdbf9065cf7b28a4928ce0512dc3b8ddc
Kathy Hudson <khitx@icloud.com>
2017-11-23 17:25:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,24 +8,6 @@ function iterate(callback) {
   return arr
 }
 
-arr.forEach(function callback(currentValue, index, array) {
-    //your iterator
-}[, thisArg])
-
 function doToArray(array, callback) {
   array.forEach(callback)
 }
-
-function doToElementsInArray(array, callback) {
-  array.forEach(callback)
-}
-
-function changeCompletely(element, index, array) {
-  array[index] = Math.floor(Math.random() * 100 + 2).toString() + ` ${array[index]}s!!!`
-}
-
-var animals = ["dog", "cat", "squirrel"];
-
-doToElementsInArray(animals, changeCompletely)
-
-console.log(animals)
